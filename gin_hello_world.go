60e0348f3df025dfe9525a7bb55aebeb9ddc460d
sayalipol <sayalip1506@gmail.com>
2023-03-12 11:24:35 +0530

    quick start gin gonic code

--- gin_hello_world.go
+++ gin_hello_world.go
@@ -18,20 +18,27 @@ func main() {
 
 	r := gin.Default()
 
+	// 1.
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
-
+	// 2.
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "I am Taylor Swift")
 	})
-
+	// 3.
 	name := "Sayali Pol"
 	r.GET("/var", func(c *gin.Context) {
 		c.String(200, name)
 	})
+	// 4.
+	data := map[string]string{"ML": "Machine Learning",
+		"AI":  "Artificial Intelligence",
+		"EMB": "Embedded Systems"}
 
-	
+	r.GET("/course", func(c *gin.Context) {
+		c.JSON(200, data)
+	})
 
 	r.Run()
 
